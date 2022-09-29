bb6c377922b6e983b300bdad5e96f448012cb07d
LeonRep <leonrepsold@gmail.com>
2022-09-29 06:35:34 +0200

    Repositories was created and implemented

--- ConnectedOffice/DeviceManagement_WebApp/Controllers/ZonesController.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Controllers/ZonesController.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using DeviceManagement_WebApp.Data;
 using DeviceManagement_WebApp.Models;
 
 namespace DeviceManagement_WebApp.Controllers
